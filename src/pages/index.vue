cca39bfbbae6124af81b7f7dc9322469ce72013b
herckalm <iraklisnd@gmail.com>
2024-06-08 20:33:33 +0300

    supabase integration

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Home Page</h1>
-  <RouterLink to="/projects">Projects Page</RouterLink>
+  <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
 </template>
 
 <script setup lang="ts">
---
+++ src/pages/projects/[...catchAll].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="error">404 Error: Page Not Found</div>
+</template>
+
+<script setup lang="js">
+import { ref } from 'vue'
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
