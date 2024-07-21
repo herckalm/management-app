94af57dd54726394e5ed83fb3688eeee034baa65
Iraklis Kalamas <iraklisnd@gmail.com>
2024-07-21 22:08:20 +0300

    layout and styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import { supabase } from '@/lib/supabaseClient'
+import TopNavBar from '@/components/layout/TopNavBar.vue'
+import SideBar from '@/components/layout/SideBar.vue'
 </script>
 
 <template>
-  <main>
-    <RouterView></RouterView>
-  </main>
+  <SideBar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavBar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/layout/SideBarLinks.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface LinkProp {
+  title: string
+  to: string
+  icon: string
+}
+
+defineProps<{
+  links: LinkProp[]
+  accountLinks: LinkProp[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="link in links"
+    :key="link.title"
+    :to="link.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+
+  <RouterLink
+    exactActiveClass="text-primary bg-muted"
+    v-for="accountLink in accountLinks"
+    :key="accountLink.title"
+    :to="accountLink.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+  >
+    <iconify-icon :icon="accountLink.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">Profile</span>
+  </RouterLink>
+</template>
